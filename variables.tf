242486df768569d9a2b722f54ced9a4b7d5ecbf5
SanjaySanghani-73 <sanjaysanghani1973@gmail.com>
2025-10-23 17:27:37 -0400

    Terraform validation: validate

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# Variables for CloudInfra Designer
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
